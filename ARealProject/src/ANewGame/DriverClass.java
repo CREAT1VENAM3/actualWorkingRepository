b850674a2524f48a9f48dc285fcf045219ab7474
CREAT1VENAM3 <rockyliseno@gmail.com>
2022-01-01 22:39:56 -0500

    commit 10:39

--- ARealProject/src/ANewGame/DriverClass.java
+++ ARealProject/src/ANewGame/DriverClass.java
@@ -5,8 +5,7 @@ public class DriverClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-		System.out.println("Please work");
-		//OHKAAAAAAAAAAAAAY this is actually working i think
+		
 	}
 
 }
